c6bb40e26188abf4a61bee1c40c5b25f1a2f16e0
johncouldbe <johndouglasb@gmail.com>
2017-04-13 22:21:33 -0400

    Initial commit. Added wordcount.

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-// your code here!
+function txtAnalyzer(){
+
+  $(':submit').click(function(e){
+    e.preventDefault();
+    var txtArray = $('.js-textField').val().replace(/[^\w\s]/g,'').split(" ");
+    console.log(txtArray);
+
+    var wordCount = txtArray.length;
+console.log(wordCount);
+
+
+
+  $('.js-wordcount').text(wordCount);
+  $('.text-report').removeClass('hidden');
+  });
+
+}
+
+$(txtAnalyzer);
